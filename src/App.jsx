8e7a1afa11bbc692c7a8be1f29fa83a113dabaf9
Jaylan <jaylansaad20@gmail.com>
2025-07-29 23:54:58 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Notelist from "./components/NoteList";
+import Noteform from "./components/NoteForm";
+import NoteView from "./components/NoteView";
+
+
+function 
+
